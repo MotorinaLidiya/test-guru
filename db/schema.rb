e13ef4092988f67cb35e0a6104bb2626b8bc6ab5
Lida Motorina <motorina.lidiya@gmail.com>
2024-08-29 16:47:19 +0300

    modified attributes Lesson 3 task 2

--- db/schema.rb
+++ db/schema.rb
@@ -13,30 +13,37 @@
 ActiveRecord::Schema.define(version: 2024_08_28_100539) do
 
   create_table "answers", force: :cascade do |t|
-    t.text "answer_body", null: false
+    t.text "body", null: false
     t.boolean "correct", default: false, null: false
+    t.integer "question_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "category_title", null: false
+    t.string "title", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "questions", force: :cascade do |t|
-    t.text "question_body", null: false
+    t.text "body", null: false
+    t.integer "test_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["test_id"], name: "index_questions_on_test_id"
   end
 
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
     t.integer "level", default: 0, null: false
-    t.string "test_author", null: false
+    t.integer "category_id", null: false
+    t.integer "author_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_id"], name: "index_tests_on_author_id"
+    t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -46,4 +53,8 @@ ActiveRecord::Schema.define(version: 2024_08_28_100539) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "answers", "questions"
+  add_foreign_key "questions", "tests"
+  add_foreign_key "tests", "categories"
+  add_foreign_key "tests", "users", column: "author_id"
 end
