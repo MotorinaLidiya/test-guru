5c5fa8fffaca2bcddea3d4964165a4667cfeba2b
Lida Motorina <motorina.lidiya@gmail.com>
2024-08-30 20:35:48 +0300

    [lesson 4] added seed

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,7 @@
 class Test < ApplicationRecord
+  def self.sorted_titles(category_name)
+    category = Category.find_by(title: category_name)
+
+    Test.where(category_id: category.id).order(id: :desc)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  def tests_by_level(level)
+    Test.where(author_id: id, level:)
+  end
 end
