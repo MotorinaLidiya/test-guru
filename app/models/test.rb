7a7e0f83cf3580b666df173f88fccde577edbf04
Lida Motorina <motorina.lidiya@gmail.com>
2024-09-02 20:48:39 +0300

    modified associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,11 +2,9 @@ class Test < ApplicationRecord
   belongs_to :category
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
-  has_many :questions
-  has_many :passed_tests
+  has_many :questions, dependent: :destroy
+  has_many :passed_tests, dependent: :destroy
   has_many :users, through: :passed_tests
-
-  validates :author, presence: true
 
   def self.sorted_titles_by_category(category_name)
     joins(:category)
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,44 +9,46 @@ users = [
 users.map! { |user_attributes| User.find_or_create_by(user_attributes) }
 
 tests = [
-  { title: 'Основы Rails', level: 1, category_id: categories.first.id, author_id: users.first.id },
-  { title: 'Основы Ruby', level: 2, category_id: categories.second.id, author_id: users.second.id },
-  { title: 'Основы SQL', level: 3, category_id: categories.third.id, author_id: users.third.id }
+  { title: 'Основы Rails', level: 1, category: categories[0], author: users[0] },
+  { title: 'Основы Ruby', level: 2, category: categories[1], author: users[1] },
+  { title: 'Основы SQL', level: 3, category: categories[2], author: users[2] }
 ]
 tests.map! { |test_attributes| Test.find_or_create_by(test_attributes) }
 
 passed_tests = [
-  { user_id: users.first.id, test_id: tests.first.id, completed: true },
-  { user_id: users.first.id, test_id: tests.second.id, completed: true },
-  { user_id: users.second.id, test_id: tests.second.id, completed: false },
-  { user_id: users.second.id, test_id: tests.third.id, completed: false }
+  { user: users[0], test: tests[0], completed: true },
+  { user: users[0], test: tests[1], completed: true },
+  { user: users[1], test: tests[1], completed: false },
+  { user: users[1], test: tests[2], completed: false },
+  { user: users[2], test: tests[0], completed: true },
+  { user: users[2], test: tests[2], completed: false }
 ]
 passed_tests.each { |passed_test_attribute| PassedTest.find_or_create_by(passed_test_attribute) }
 
   questions = [
-  { body: 'Что такое Active Record в Rails?', test_id: tests.first.id },
-  { body: 'Какой файл содержит маршруты в приложении Rails?', test_id: tests.first.id },
-  { body: 'Что делает метод map в Ruby?', test_id: tests.second.id },
-  { body: 'Какой SQL-запрос используется для выборки данных?', test_id: tests.third.id },
-  { body: 'Что делает оператор JOIN в SQL?', test_id: tests.third.id }
+  { body: 'Что такое Active Record в Rails?', test: tests[0] },
+  { body: 'Какой файл содержит маршруты в приложении Rails?', test: tests[0] },
+  { body: 'Что делает метод map в Ruby?', test: tests[1] },
+  { body: 'Какой SQL-запрос используется для выборки данных?', test: tests[2] },
+  { body: 'Что делает оператор JOIN в SQL?', test: tests[2] }
 ]
 questions.map! { |question_attributes| Question.find_or_create_by(question_attributes) }
 
 answers = [
-  { body: 'Это компонент для управления представлениями', correct: false, question_id: questions.first.id },
-  { body: 'Это система управления базами данных', correct: false, question_id: questions.first.id },
-  { body: 'Это ORM-библиотека для работы с базами данных', correct: true, question_id: questions.first.id },
-  { body: 'routes.rb', correct: true, question_id: questions.second.id },
-  { body: 'config.yml', correct: false, question_id: questions.second.id },
-  { body: 'application.rb', correct: false, question_id: questions.second.id },
-  { body: 'Выводит элементы массива', correct: false, question_id: questions.third.id },
-  { body: 'Преобразует каждый элемент массива и возвращает новый массив', correct: true, question_id: questions.third.id },
-  { body: 'Удаляет элементы из массива', correct: false, question_id: questions.third.id },
-  { body: 'SELECT', correct: true, question_id: questions.fourth.id },
-  { body: 'INSERT', correct: false, question_id: questions.fourth.id },
-  { body: 'DELETE', correct: false, question_id: questions.fourth.id },
-  { body: 'Удаляет таблицу', correct: false, question_id: questions.fifth.id },
-  { body: 'Объединяет данные из нескольких таблиц', correct: true, question_id: questions.fifth.id },
-  { body: 'Изменяет структуру таблицы', correct: false, question_id: questions.fifth.id }
+  { body: 'Это компонент для управления представлениями', correct: false, question: questions[0] },
+  { body: 'Это система управления базами данных', correct: false, question: questions[0] },
+  { body: 'Это ORM-библиотека для работы с базами данных', correct: true, question: questions[0] },
+  { body: 'routes.rb', correct: true, question: questions[1] },
+  { body: 'config.yml', correct: false, question: questions[1] },
+  { body: 'application.rb', correct: false, question: questions[1] },
+  { body: 'Выводит элементы массива', correct: false, question: questions[2] },
+  { body: 'Преобразует каждый элемент массива и возвращает новый массив', correct: true, question: questions[2] },
+  { body: 'Удаляет элементы из массива', correct: false, question: questions[2] },
+  { body: 'SELECT', correct: true, question: questions[3] },
+  { body: 'INSERT', correct: false, question: questions[3] },
+  { body: 'DELETE', correct: false, question: questions[3] },
+  { body: 'Удаляет таблицу', correct: false, question: questions[4] },
+  { body: 'Объединяет данные из нескольких таблиц', correct: true, question: questions[4] },
+  { body: 'Изменяет структуру таблицы', correct: false, question: questions[4] }
 ]
 answers.each { |answer_attribute| Answer.find_or_create_by(answer_attribute) }
