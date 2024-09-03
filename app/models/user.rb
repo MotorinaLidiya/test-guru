ded67cd4ffbbb481a67a944e6aa60577b21f8d1b
Lida Motorina <motorina.lidiya@gmail.com>
2024-09-03 13:37:07 +0300

    changed to "destroy"

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :passed_tests, dependent: :destroy
   has_many :tests, through: :passed_tests
-  has_many :authored_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :nullify
+  has_many :authored_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :destroy
 
   def tests_by_level(level)
     tests.where(level:)
