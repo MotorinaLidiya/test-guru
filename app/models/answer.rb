863b4301594be028030f66b8ce5baed173191a81
Lida Motorina <motorina.lidiya@gmail.com>
2024-09-04 16:51:56 +0300

    refactoring

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,15 +2,13 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   validates :body, presence: true
-  validate :validate_answers_quantity
+  validate :validate_answers_quantity, on: :create
 
   scope :correct, -> { where(correct: true) }
 
   private
 
   def validate_answers_quantity
-    return unless question_id_changed?
-
     total_answers = question.answers.count
     errors.add(:question_answers, 'должно быть от 1 до 4 ответов к вопросу') if total_answers >= 4
   end
