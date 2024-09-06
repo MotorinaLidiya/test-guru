aaaa8bd333fe0fa9a38688e682ab1a6087ec7e66
Lida Motorina <motorina.lidiya@gmail.com>
2024-09-07 02:46:39 +0300

    [lesson 8] added lesson 8

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,18 @@
 module QuestionsHelper
+  def question_header(test)
+    title = test.title || "Неизвестный тест"
+    if @question.persisted?
+      "Редактировать вопрос теста: #{title}"
+    else
+      "Создать вопрос для теста: #{title}"
+    end
+  end
+
+  def current_year
+    "Год: #{Time.now.year}"
+  end
+
+  def github_url(author, repo)
+    "https://github.com/#{author}/#{repo}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'tests#index'
 
-  resources :tests, only: [] do
+  resources :tests, only: [:index] do
     resources :questions, shallow: true
   end
 end
