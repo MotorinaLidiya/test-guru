0fcba2ac97e0113c07b4f80c9da84531ffb51fdc
Lida Motorina <motorina.lidiya@gmail.com>
2024-09-16 21:42:11 +0300

    [lesson 12] added lesson

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -19,7 +19,7 @@ class Admin::QuestionsController < Admin::BaseController
   def create
     @question = @test.questions.build(question_params)
     if @question.save
-      redirect_to admin_question_path(@question), notice: 'Вопрос успешно добавлен'
+      redirect_to admin_question_path(@question), notice: t('.success')
     else
       render :new
     end
@@ -27,7 +27,7 @@ class Admin::QuestionsController < Admin::BaseController
 
   def update
     if @question.update(question_params)
-      redirect_to admin_question_path(@question), notice: 'Вопрос успешно изменен'
+      redirect_to admin_question_path(@question), notice: t('.success')
     else
       render :edit
     end
@@ -35,7 +35,7 @@ class Admin::QuestionsController < Admin::BaseController
 
   def destroy
     @question.destroy
-    redirect_to admin_test_path(@question.test)
+    redirect_to admin_test_path(@question.test), notice: t('.success')
   end
 
   private
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  before_action :set_locale
+
+  private
+
   def after_sign_in_path_for(resource)
     resource.is_a?(Admin) ? admin_tests_path : super
   end
@@ -10,5 +14,17 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+  end
+
+  def default_url_options
+    if I18n.locale != I18n.default_locale
+      { locale: I18n.locale }
+    else
+      {}
+    end
+  end
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
   end
 end
